d74f13ec6c0597d0a749d35eba56374c21b9f5ab
TorGuard <84390765+torguardvpn@users.noreply.github.com>
2025-02-20 23:15:58 -0500

    Update tgwireguard2.lua

--- files/usr/lib/lua/luci/controller/admin/tgwireguard2.lua
+++ files/usr/lib/lua/luci/controller/admin/tgwireguard2.lua
@@ -1,4 +1,4 @@
 module("luci.controller.admin.tgwireguard2", package.seeall)
 function index()
-	entry({"admin", "vpn", "tgwireguard2"}, cbi("torguard/tgwireguard2"), _("TorGuard Servers"), 102)
+	entry({"admin", "vpn", "tgwireguard2"}, cbi("torguard/tgwireguard2"), _("TorGuard Wireguard"), 102)
 end
